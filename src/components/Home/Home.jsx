f6fd9eccbe4a790d6c9dc529649ebdcd6be7c43f
Mekha <mekhalnair@gmail.com>
2023-10-06 00:33:30 +0530

    adding pagination

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,10 +2,10 @@ import React, { useState } from "react";
 import "../Home/Home.css";
 import Card from "../Cards/Card";
 import data from "../../data";
-
+import Pagination from "../Pagination";
 const Home = (props) => {
   const [currentPage, setCurrentpage] = useState(1);
-  const [postsPerPage, setPostsPerPage] = useState(6);
+  const [postsPerPage] = useState(6);
   const searchValue = props.values;
   const indexOfLastPost = currentPage * postsPerPage;
   const indexOfFirstPost = indexOfLastPost - postsPerPage;
@@ -16,12 +16,22 @@ const Home = (props) => {
       )
     : currentPosts;
 
+  //change page
+  const paginate=(pageNumber)=>setCurrentpage(pageNumber)
+
+
+
+
   return (
-    <div className="cards-wrapper">
+    <>
+     <div className="cards-wrapper">
       {filteredData.map((item) => (
         <Card key={item.id} value={item} />
       ))}
     </div>
+   <Pagination postsPerPage={postsPerPage} totalPosts={data.length} paginate={paginate}/>
+    </>
+   
   );
 };
 
