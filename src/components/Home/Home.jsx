5cd70910f6d2e3d4cac4f3dc6e69b773a28c2b7d
Mekha <mekhalnair@gmail.com>
2023-10-05 23:48:01 +0530

    gonna add pagination

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "../Home/Home.css";
 import Card from "../Cards/Card";
 import data from "../../data";
 
 const Home = (props) => {
+  const [currentPage, setCurrentpage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(6);
   const searchValue = props.values;
- 
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = data.slice(indexOfFirstPost, indexOfLastPost);
   const filteredData = searchValue
     ? data.filter((item) =>
         item.name.toLowerCase().includes(searchValue.toLowerCase())
       )
-    : data;  
+    : currentPosts;
 
   return (
     <div className="cards-wrapper">
