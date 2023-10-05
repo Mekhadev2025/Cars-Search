8a7e1dcad2f178540fda4f362f47ccc4f9a8ae4f
Mekha <mekhalnair@gmail.com>
2023-10-05 20:22:25 +0530

    adding burron

--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -12,6 +12,17 @@ const Card = (props) => {
         
       <img src={props.value.image} className='carCard'></img>
       <h2 className='carTitle'>{props.value.name}</h2>
+      <div className="detailsGrid">
+        <div className="grid capacity">{props.value.capacity} People</div>
+        <div className="grid fuel">{props.value.fuel}</div>
+        <div className="grid mode">{props.value.mode}</div>
+        <div className="grid type">{props.value.type}</div>
+      </div>
+      <div className='prizeContainer'>
+        <div className='prizeCar'>${props.value.price}<span className='monthPrize'>/month</span></div>
+        <div className='likeBtn'></div>
+        <button className='rentBtn'>Rent Now</button>
+      </div>
     </div>
   )
 }
